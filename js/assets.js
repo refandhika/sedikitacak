fbfbd767ef6a0685a1900a94e84c3839a124fa2b
Refa Andhika <refandika@gmail.com>
2016-07-16 01:44:38 +0700

    Add text when hover.

--- js/assets.js
+++ js/assets.js
@@ -9,11 +9,13 @@ $(document).ready(function(){
 		newLeft = curLeft - 90;
 		if (!$(this).hasClass('animated')) {
 			$(this).dequeue().stop().animate({ borderRadius:"200px", height:"200px", width: "200px", top: newTop+"px", left: newLeft+"px"});
+			$('.in-text').animate({ marginTop: "91px" });
 		}
 	},	
 	function() {
 		newTop = Math.floor(Math.random() * 500) + 90
 		newLeft = Math.floor(Math.random() * 1170) + 90
+		$('.in-text').animate({ marginTop: "1px" });
 	   	$(this).addClass('animated').animate({ borderRadius:"20px", height:"20px", width: "20px", top: newTop+"px", left: newLeft+"px"}, "normal", "linear", function() {
 				$(this).removeClass('animated').dequeue();
 			});
