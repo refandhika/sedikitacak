5f12db6a868e21f5d8ede1b77267a92f591c9742
refandhika <refandika@gmail.com>
2017-04-10 13:54:40 +0700

    Migrate to React

---
+++ assets/js/firebase.init.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDGzavlWXPi20ylHp5D-h6wiYKXEaFj-K4",
+    authDomain: "sedikit-acak.firebaseapp.com",
+    databaseURL: "https://sedikit-acak.firebaseio.com",
+    storageBucket: "sedikit-acak.appspot.com",
+    messagingSenderId: "620762660097"
+};
+firebase.initializeApp(config);
