94436681c96bc13cc314623cd772c1db4f4c9d33
refandhika <refandika@gmail.com>
2017-08-22 03:42:45 +0700

    Update js not found button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var stflag = false;
 	var smi = document.getElementById("skill-more-info");
 	var st = document.getElementById("btn-skill-toggle");
-	st.addEventListener("click", function(){
-		if(!stflag){
-			var hs = document.getElementsByClassName("skill-hidden");
-			for(var i=0; i<hs.length; i++){
-			    hs[i].style.display = "inline-block";
+	if(st!=null){
+		st.addEventListener("click", function(){
+			if(!stflag){
+				var hs = document.getElementsByClassName("skill-hidden");
+				for(var i=0; i<hs.length; i++){
+				    hs[i].style.display = "inline-block";
+				}
+				smi.innerHTML = "Now you had seen it all. What do you think?"
+				st.innerHTML = "Hide";
+				stflag = true;
+			} else {
+				var hs = document.getElementsByClassName("skill-hidden");
+				for(var i=0; i<hs.length; i++){
+				    hs[i].style.display = "none";
+				}
+				smi.innerHTML = "Why did you hide it in the first place if you want to show it again?"
+				st.innerHTML = "Show Me Again!";
+				stflag = false;
 			}
-			smi.innerHTML = "Now you had seen it all. What do you think?"
-			st.innerHTML = "Hide";
-			stflag = true;
-		} else {
-			var hs = document.getElementsByClassName("skill-hidden");
-			for(var i=0; i<hs.length; i++){
-			    hs[i].style.display = "none";
-			}
-			smi.innerHTML = "Why did you hide it in the first place if you want to show it again?"
-			st.innerHTML = "Show Me Again!";
-			stflag = false;
-		}
-		scrollToTop(document.body, 0, 1000);
-	});
+			scrollToTop(document.body, 0, 1000);
+		});
+	}
 });
 
 var scrollSpeed = 10;
