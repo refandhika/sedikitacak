b16747476cdefb9fac304e7a5d595aae0196fe03
refandhika <refandika@gmail.com>
2017-08-22 03:37:42 +0700

    Changing Web To Golang

--- assets/js/assets.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-	var today = moment().format('MMMM Do YYYY');
-	$('.left-foot').text(today);
-
-	var year = moment().format('YYYY');
-	$('.year').text(year);
-
-});
--- assets/js/firebase.init.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDGzavlWXPi20ylHp5D-h6wiYKXEaFj-K4",
-    authDomain: "sedikit-acak.firebaseapp.com",
-    databaseURL: "https://sedikit-acak.firebaseio.com",
-    storageBucket: "sedikit-acak.appspot.com",
-    messagingSenderId: "620762660097"
-};
-firebase.initializeApp(config);
